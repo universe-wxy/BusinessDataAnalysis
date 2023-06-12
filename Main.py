193891cba68ec00e192055180e0503ca0f810f28
王熙宇 <2537239220@qq.com>
2023-06-12 18:56:35 +0800

    StudentPerformance

--- Main.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df=pd.read_stata('./CGSS2021.dta',convert_categoricals=False, convert_missing=True)
-respondent=df.loc[:,['A7a','A2','A3_1','A10','A18','A43','A8a','A58']]
-print(df.head())
